01d9d78b90be5c9b0f7c2ed608d81912d30982ca
tamorimoto <tamorimoto@hotmail.com>
2020-03-14 23:10:25 +0900

    restore original

--- src/main/java/com/aws/codestar/projecttemplates/GatewayResponse.java
+++ src/main/java/com/aws/codestar/projecttemplates/GatewayResponse.java
@@ -14,13 +14,14 @@ public class GatewayResponse {
     private final String body;
     private final Map<String, String> headers;
     private final int statusCode;
-    private final List<String> languages;
+    // private final List<String> languages;
 
-    public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode, List<String> languages) {
+    // public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode, List<String> languages) {
+    public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode) {
         this.statusCode = statusCode;
         this.body = body;
         this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
-        this.languages = Collections.unmodifiableList(new ArrayList<>(languages));
+        // this.languages = Collections.unmodifiableList(new ArrayList<>(languages));
     }
 
     public String getBody() {
@@ -35,7 +36,7 @@ public class GatewayResponse {
         return statusCode;
     }
 
-    public List<String> getLanguages() {
-        return languages;
-    }
+    // public List<String> getLanguages() {
+    //     return languages;
+    // }
 }
--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -17,12 +17,13 @@ import java.util.List;
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
-        System.out.println("input:" + input);
+        // System.out.println("input:" + input);
         Map<String, String> headers = new HashMap<>();
-        // headers.put("Content-Type", "application/json");
+        headers.put("Content-Type", "application/json");
         // List<String> languages = Arrays.asList("Java", "Ruby", "python");
         // return new GatewayResponse(new JSONObject().put("Output", "Hello World! Hello everyone!!").toString(), headers, 200, languages);
-        headers.put("Content-Type", "text/html");
-        return "<!DOCTYPE html><html lang=\"ja\"><head><meta charset=\"UTF-8\"><title>my page</title><body><h3>Hello, World!!</h3></body></html>";
+        return new GatewayResponse(new JSONObject().put("Output", "Hello World! Hello everyone!!").toString(), headers, 200);
+        // headers.put("Content-Type", "text/html");
+        // return "<!DOCTYPE html><html lang=\"ja\"><head><meta charset=\"UTF-8\"><title>my page</title><body><h3>Hello, World!!</h3></body></html>";
     }
 }
